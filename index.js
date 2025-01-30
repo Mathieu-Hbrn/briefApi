f575119e368d07badcd1458c9abebe84aa8864e6
mathieu.houbron <mathieu.houbron@campus-centre.fr>
2025-01-30 18:19:40 +0100

    Le POST fonctionne enfin! (erreur de synthaxe dans la requète...)

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let produits = [
     { id: 2, nom: "Café Arabica", prix: 8.99, quantite: 20 },
 ];
 
+
 // Routes
 // GET /produits
 // Liste des produits
@@ -17,8 +18,16 @@ app.get("/produits", (req,res) => {
     res.json(produits);
 })
 
+// POST
+app.post('/produits', (req, res) => {
+    produits.push(req.body);
+    res.status(201).json(produits);
+});
+
 
 // Démarrage du serveur
 app.listen(port, () =>{
     console.log(`Le serveur est démarré suur https://localhost:${port}`);
 });
+
+
